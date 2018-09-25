f314b1fedbde55a830eac19075cdcab22b3a736b
Karol Siwek <siwek.karol1992@gmail.com>
2018-09-25 16:21:13 +0200

    zmiany...

--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,15 @@ public class Main {
 
     public static void main(String[] args) {
         int i = 0;
+        int suma = 0;
         do {
             System.out.println(i * 2);
+            suma = suma + i*2;
             i++;
+
         } while (i < 51);
 
-        int suma = i++;
         System.out.println("Suma wszystkich powyższych liczb wynosi:");
-        
+        System.out.println(suma);
     }
 }
