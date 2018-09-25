f2b095dea9a38ca165f9643a3038c6dcb7c312de
KarolSiwek <siwek.karol1992@gmail.com>
2018-09-25 12:08:17 +0200

    Zadanie z dodawanie z przedziału 0 --> 100 ver.2

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,13 @@
 public class Main {
 
     public static void main(String[] args) {
-	int i = 0;
-	do {
-        System.out.println(i*2);
-        i++;
-    } while (i < 51);
+        int i = 0;
+        do {
+            System.out.println(i * 2);
+            i++;
+        } while (i < 51);
 
-	    int suma = i++;
+        int suma = i++;
         System.out.println("Suma wszystkich powyższych liczb wynosi:");
         
     }
